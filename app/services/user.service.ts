2dfc10250b82b7c166f4d562a04a0faaecfc034f
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 00:48:14 +0530

    feat: integrate app/services/user.service.ts

---
+++ app/services/user.service.ts
@@ -0,0 +1,1 @@
+import { Injectable } from '@angular/core'; @Injectable({ providedIn: 'root' }) export class UserService { private apiUrl = 'https://jsonplaceholder.typicode.com/users'; async getUsers(): Promise<any> { const response = await fetch(this.apiUrl); return response.json(); } }
